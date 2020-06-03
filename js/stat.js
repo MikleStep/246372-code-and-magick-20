a520707ebf68ce1c195cbbba6b692fade0cedec1
MikleStep <raiving_rabbid@mail.ru>
2020-06-03 14:48:47 +0300

    Исправил насыщеность и названия переменных. Добавил выравнивание по центру и вынес все отрисовки в отдельные функции. Исправил округление по математическим правилам. Убрал лишний цикл проверки.

--- js/stat.js
+++ js/stat.js
@@ -5,7 +5,7 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
-var BAR_HEIGHT = 150;
+var MAX_BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
 var BAR_GAP = 50;
 var TEXT_HEIGHT = 15;
@@ -34,30 +34,44 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
-window.renderStatistics = function (ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+var renderText = function (ctx, text, x, y) {
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили!', (CLOUD_WIDTH / 2) + TITLE_GAP, CLOUD_Y + TITLE_GAP);
-  ctx.fillText('Список результатов:', (CLOUD_WIDTH / 2) + TITLE_GAP / 2, CLOUD_Y + TITLE_GAP * 2);
-  var maxTime = getMaxElement(times);
+  ctx.textAlign = 'center';
+  ctx.fillText(text, x, y);
+};
 
+var getRandomColor = function (max) {
+  return Math.floor(Math.random() * Math.floor(max));
+};
+
+var renderGraphs = function (ctx, players, times) {
   for (var i = 0; i < players.length; i++) {
-    var barPositionX = CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i;
-    var barHeight = (BAR_HEIGHT * times[i]) / maxTime;
-    ctx.fillStyle = '#000';
-    ctx.fillText(players[i], barPositionX, CLOUD_HEIGHT - GAP * 2);
-    ctx.fillText(Math.ceil(times[i]), barPositionX, CLOUD_Y + TITLE_HEIGHT + TEXT_HEIGHT + BAR_HEIGHT - barHeight - GAP * 2);
+    var maxTime = getMaxElement(times);
+    var firstGap = (CLOUD_WIDTH - ((BAR_GAP + BAR_WIDTH) * players.length - BAR_WIDTH)) / 2
+    var barPositionX = CLOUD_X + firstGap + (BAR_GAP + BAR_WIDTH) * i;
+    var textPositionX = barPositionX + BAR_WIDTH / 2;
+    var barHeight = (MAX_BAR_HEIGHT * times[i]) / maxTime;
+    var barPositionY = CLOUD_Y + TITLE_HEIGHT + TEXT_HEIGHT + MAX_BAR_HEIGHT - barHeight;
+
     if (players[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      ctx.fillRect(barPositionX, CLOUD_Y + TITLE_HEIGHT + TEXT_HEIGHT + BAR_HEIGHT - barHeight, BAR_WIDTH, barHeight);
+      ctx.fillRect(barPositionX, barPositionY, BAR_WIDTH, barHeight);
     } else {
-      for (var j = 0; j < players.length; j++) {
-        ctx.fillStyle = 'hsl(225,' + (100 - 30 * i) + '%, 50%)';
-        ctx.fillRect(barPositionX, CLOUD_Y + TITLE_HEIGHT + TEXT_HEIGHT + BAR_HEIGHT - barHeight, BAR_WIDTH, barHeight);
-      }
+
+      ctx.fillStyle = 'hsl(225,' + (getRandomColor(100)) + '%, 50%)';
+      ctx.fillRect(barPositionX, barPositionY, BAR_WIDTH, barHeight);
     }
+    renderText(ctx, players[i], textPositionX, CLOUD_HEIGHT - GAP * 2);
+    renderText(ctx, Math.round(times[i]), textPositionX, barPositionY - GAP * 2);
   }
+};
+
+window.renderStatistics = function (ctx, players, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  renderText(ctx, 'Ура вы победили!', CLOUD_WIDTH / 2 + CLOUD_X, CLOUD_Y + TITLE_GAP);
+  renderText(ctx, 'Список результатов:', CLOUD_WIDTH / 2 + CLOUD_X, CLOUD_Y + TITLE_GAP * 2);
+  renderGraphs(ctx, players, times);
 };
