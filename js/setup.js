a35a24526498b369dc84abdfd19d2a78cdd1e1a6
MikleStep <raiving_rabbid@mail.ru>
2020-06-14 14:55:35 +0300

    Добавлен проверка валидации формы

--- js/setup.js
+++ js/setup.js
@@ -93,3 +93,28 @@ setupClose.addEventListener('keydown', function (evt) {
     closePopup();
   }
 });
+
+
+// Задание 4.1 Проверка валидации
+
+var userNameInput = document.querySelector('.setup-user-name');
+
+userNameInput.addEventListener('invalid', function () {
+  if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+userNameInput.addEventListener('input', function () {
+  var valueLength = userNameInput.value.length;
+
+  if (valueLength < MIN_NAME_LENGTH) {
+    userNameInput.setCustomValidity('Ещё ' + (MIN_NAME_LENGTH - valueLength) + ' симв.');
+  } else if (valueLength > MAX_NAME_LENGTH) {
+    userNameInput.setCustomValidity('Удалите лишние ' + (valueLength - MAX_NAME_LENGTH) + ' симв.');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
